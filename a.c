20c8ffa920ba9afa8318be9634873a75572dc404
Zhang Haifeng <1205526696@qq.com>
2023-07-24 11:45:45 +0800

    first

---
+++ a.c
@@ -0,0 +1,5 @@
+#include "stdio.h"
+void print(void)
+{
+    printf("12345\r\n");
+}
---
+++ learn.c
@@ -0,0 +1,11 @@
+#include "stdio.h"
+#include "a.h"
+int main(void)
+{
+    int i = 0;
+    for(i = 0;i<5;i++)
+    {
+        printf("i = %d\r\n",i);
+    }
+    print();
+}
